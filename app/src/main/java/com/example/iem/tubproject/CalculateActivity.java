f4d3f0c04350c618fdcee68de09f76c7f4780db1
mgaucher <marti.gaucher@hotmail.fr>
2017-02-28 17:37:56 +0100

    adding timepicjer + starting dev btnCalculate

--- app/src/main/java/com/example/iem/tubproject/CalculateActivity.java
+++ app/src/main/java/com/example/iem/tubproject/CalculateActivity.java
@@ -1,12 +1,15 @@
 package com.example.iem.tubproject;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 import com.example.iem.tubproject.Models.Line;
@@ -19,6 +22,7 @@ import retrofit2.Callback;
 import retrofit2.Response;
 
 import static android.R.attr.data;
+import static android.R.attr.timePickerDialogTheme;
 import static com.example.iem.tubproject.rest.ApiClient.getApiInterface;
 
 
@@ -27,15 +31,19 @@ public class CalculateActivity extends AppCompatActivity {
     Spinner spChooseLigne ;
     Spinner spStartStop;
     Spinner spFinishStop;
-
+    TimePicker timePicker;
+    Button btnCalculate;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        setContentView(R.layout.activity_calculate);
+
         getLines(this);
 
+        timePicker = (TimePicker) findViewById(R.id.timePicker);
+        timePicker.setIs24HourView(true);
 
-        setContentView(R.layout.activity_calculate);
         spChooseLigne = (Spinner) findViewById(R.id.SpinchooseLigne);
         spStartStop = (Spinner) findViewById(R.id.SpinchooseLigne);
         spChooseLigne.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
@@ -47,6 +55,33 @@ public class CalculateActivity extends AppCompatActivity {
 
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        btnCalculate = (Button) findViewById(R.id.btnCalculate);
+        btnCalculate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (spFinishStop.getSelectedItem() != null && spStartStop.getSelectedItem() != null && spChooseLigne.getSelectedItem() != null
+                        ) {
+                    timePicker.clearFocus();
+
+                    Intent myIntent = new Intent(CalculateActivity.this, ResultCalculateActivity.class);
+                    myIntent.putExtra("startStop",spStartStop.getSelectedItem().toString());
+                    myIntent.putExtra("finishStop",spFinishStop.getSelectedItem().toString());
+                    Line line = (Line)spChooseLigne.getSelectedItem();
+                    myIntent.putExtra("numLine",line.getIdLine());
+                    myIntent.putExtra("hour",timePicker.getCurrentHour());
+                    myIntent.putExtra("minute",timePicker.getCurrentMinute());
+                    startActivity(myIntent);
+                }
+                else
+                {
+                    Toast.makeText(CalculateActivity.this,"Veuillez renseigner toute les informations",
+                            Toast.LENGTH_LONG).show();
+                }
+                // force the timepicker to loose focus and the typed value is available !
 
             }
         });
@@ -66,13 +101,12 @@ public class CalculateActivity extends AppCompatActivity {
                                  Toast.makeText(context,"Pas de ligne disponible",
                                          Toast.LENGTH_LONG).show();
                              }
-                             else
-                             {
+
                                  spChooseLigne = (Spinner) findViewById(R.id.SpinchooseLigne);
                                  ArrayAdapter spinnerArrayAdapter = new ArrayAdapter(context,
                                          android.R.layout.simple_spinner_item, response.body());
                                  spChooseLigne.setAdapter(spinnerArrayAdapter);
-                             }
+
                          }
 
                          @Override
@@ -97,14 +131,13 @@ public class CalculateActivity extends AppCompatActivity {
                                  Toast.makeText(context,"Pas d'arrêt disponible",
                                          Toast.LENGTH_LONG).show();
                              }
-                             else{
+
                                  spStartStop = (Spinner) findViewById(R.id.etDepart);
                                  spFinishStop= (Spinner) findViewById(R.id.etArrivee);
                                  ArrayAdapter spinnerArrayAdapter = new ArrayAdapter(context,
                                          android.R.layout.simple_spinner_item, response.body());
                                  spStartStop.setAdapter(spinnerArrayAdapter);
                                  spFinishStop.setAdapter(spinnerArrayAdapter);
-                             }
 
                          }
 
--- app/src/main/java/com/example/iem/tubproject/ResultCalculateActivity.java
+++ app/src/main/java/com/example/iem/tubproject/ResultCalculateActivity.java
@@ -1,7 +1,9 @@
 package com.example.iem.tubproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class ResultCalculateActivity extends AppCompatActivity {
 
@@ -9,5 +11,17 @@ public class ResultCalculateActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result_calculate);
+
+
+        Intent myIntent = getIntent();
+        String startStop = myIntent.getStringExtra("startStop");
+        String finishStop= myIntent.getStringExtra("finishStop");
+        String numLine= myIntent.getStringExtra("numLine");
+        String hour= myIntent.getStringExtra("hour");
+        String minute= myIntent.getStringExtra("minute");
+
+
+        Toast.makeText(this,startStop+finishStop+numLine+hour+minute,
+                Toast.LENGTH_LONG).show();
     }
 }
